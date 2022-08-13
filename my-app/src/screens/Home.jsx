7099f0a2568969d276215a49948cb091c1fa9ef0
Ahmad haq <106380578+ahmadhaqofficial@users.noreply.github.com>
2022-08-13 06:28:10 -0700

    yrr bohat khap gya wan

--- my-app/src/screens/Home.jsx
+++ my-app/src/screens/Home.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import logo from '../assets/navbarlogo.png'
 import background from '../assets/backgroudnavbar.png'
 import navright from '../assets/navright.png'
+import card from '../assets/card.png'
+
 
 
 export default function Home() {
@@ -10,6 +12,7 @@ export default function Home() {
       <div className='img_nav'>
       <img className='body__img' src={background} alt="" />
       </div>
+      <div className='rlb__project__all'>
         <div className='rlb__project__navbar'>
             <div className='rlb__project__navbar__logo'>
     <img src={logo} alt="" />
@@ -24,32 +27,76 @@ export default function Home() {
 </div>
 
         </div >
-        <div className='rlb__project__content'>
-        <div className='rlb__project__content__one'>
-          <div className='rlb__project__heading'>
-            Get the Best Job <br /><span>You Deserve</span>
-          </div>
-          <div className='rlb__project__sub__heading'>
+        <div className='rlb__content__main'>
+        <div className=' rlb__content__main__left'>
+          <div className='rlb__heading'>
+          Get the Best Job <br />
+<span>You Deserve</span>
+          </div >
+          <div className='rlb__content'>
           Tell us who you are and let us <br />
-find the best job for you
+find the best job for you!
           </div>
-          <div className='rlb__project__content__button'>
-            <button className='nav__buton'>
-              see more
-            </button>
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-right"><line x1="5" y1="12" x2="19" y2="12"></line><polyline points="12 5 19 12 12 19"></polyline></svg>
+          <div className='rlb__content__btn'>
+            <button className='btn'>See more</button>
+            
+<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-right"><line x1="5" y1="12" x2="19" y2="12"></line><polyline points="12 5 19 12 12 19"></polyline></svg>
           </div>
         </div>
-        <div className='rlb__project__navbar__right__logo'>
-<img src={navright} alt="" />
-        </div>
-        </div>
-        <div className='choose__friend__container'>
-          Choose your friend
+        <div>
+          <img className='img__content' src={navright} alt="" />
         </div>
      
 
 
     </div>
+    </div>
+    
+<div className='choose__friend'>
+Choose your friend
+</div>
+<div className='choose__friend__all'>
+<div className='choose_card'>
+<img src={card} alt="" />
+<button className='manager__button'>Manager Field Service</button>
+
+</div>
+<div className='choose_card'>
+<img src={card} alt="" />
+<button className='manager__button'>Manager Field Service</button>
+
+</div>
+<div className='choose_card'>
+<img src={card} alt="" />
+<button className='manager__button'>Manager Field Service</button>
+
+</div>
+</div>
+<div className='choose__friend__all'>
+<div className='choose_card'>
+<img src={card} alt="" />
+<button className='manager__button'>Manager Field Service</button>
+
+</div>
+<div className='choose_card'>
+<img src={card} alt="" />
+<button className='manager__button'>Manager Field Service</button>
+
+</div>
+<div className='choose_card'>
+<img src={card} alt="" />
+<button className='manager__button'>Manager Field Service</button>
+
+</div>
+</div>
+<div className='find__button'>
+  <button>Find 1676 Technical Vacanciess</button>
+</div>
+
+
+
+
+    </div>
+
   )
 }
