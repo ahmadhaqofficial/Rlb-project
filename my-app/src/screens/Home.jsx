827c72bae2e801676e12042cabb4acdf1fd7a002
Ahmad haq <106380578+ahmadhaqofficial@users.noreply.github.com>
2022-08-13 12:40:03 -0700

    responsive

--- my-app/src/screens/Home.jsx
+++ my-app/src/screens/Home.jsx
@@ -39,9 +39,9 @@ find the best job for you!
           </div>
           <div className='rlb__content__btn'>
             <button className='btn'>See more</button>
-            
+            <div className='rlb__content__img'>
 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-right"><line x1="5" y1="12" x2="19" y2="12"></line><polyline points="12 5 19 12 12 19"></polyline></svg>
-          </div>
+          </div></div>
         </div>
         <div>
           <img className='img__content' src={navright} alt="" />
